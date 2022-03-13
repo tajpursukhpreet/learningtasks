57ad9b7b39410d3de4e3ab479eb506aeea24f6c2
tajpursukhpreet <100057973+tajpursukhpreet@users.noreply.github.com>
2022-03-14 00:38:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <ul>
     <li><a href="index.php" target="_blank"> HOME </a></li>
     <li><a href="/LT2/Homepage" target= "_blank"> LEARNING TASK 2 </a> </li>
+      <li><a href="/LT3/Index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
   </ul>
   </body>
 </html>
