7d631d018fc37851c3e3cf35dbf38fbcc33c8c2b
tajpursukhpreet <100057973+tajpursukhpreet@users.noreply.github.com>
2022-05-21 22:29:54 +0800

    Add files via upload

---
+++ CLE1/LT2/homepage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> Welcome to My Website! I am Sukhpreet Tajpur </h1>
+  <ul>
+    <li><a href="index.php" target="_blank"> HOME </a></li>
+    <li><a href="beef in beer.html" target= "_blank"> beef in beer </a> </li>
+    <li><a href="cucumbersalad.html" target= "_blank"> cucumbersalad </a> </li>
+    <li><a href="eggfriedrice.html" target= "_blank"> eggfriedrice </a> </li>
+    <li><a href="goat cheese salad.html" target= "_blank">goat cheese salad </a> </li>
+    <li><a href="omelette.html" target= "_blank"> omelette </a> </li>
+    <li><a href="salmon in the oven.html" target= "_blank"> salmon in the oven </a> </li>
+    <li><a href="thai green curry.html" target= "_blank"> thai green curry </a> </li>
+  </ul>
+  </body>
+</html>
